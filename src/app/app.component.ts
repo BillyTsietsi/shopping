346dbfbd11ccf514c3401838d8b7ed5a6c535a05
Billy <tb.mokolokoto@gmail.com>
2019-06-14 13:09:09 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping';
+  NewPro = [];
+  Price : number = 0;
+  ProductNumber: number =0;
+  TotalQuantity: number = 0;
+
+  productList = [
+    {
+      proName: 'Samsung Galaxy S8', price : 11000, limit : 10,quantity : 0, image:'/assets/Samsung Galaxy S6 .jpg'
+    },
+    {
+      proName: 'Huawei P30 Pro.jpg', price : 18000, limit : 10,quantity : 0, image:'assets/Huawei P30 Pro.jpg'
+    },
+    {
+      proName: 'Samsung Galaxy J7Star Blue', price : 31000, limit : 10,quantity : 0, image:'/assets/Samsung Galaxy J7Star Blue.jpg'
+    },
+    {
+      proName: 'Huawei P20 Pro', price : 9000, limit : 10,quantity : 0, image:'/assets/Huawei P20 Pro.jpg'
+    }
+  ]
+
+
+  quantity_inc(list){
+
+ this.NewPro.push(list)
+
+ this.ProductNumber= this.NewPro.length;
+
+    if(list.limit == list.quantity){
+      return alert("Your cart has reached muximum limit")
+    }else{
+      list.quantity++
+      this.Price += list.price
+      
+      
+    }
+  }
+
+  quantity_dec(list){
+
+   let index = this.NewPro.indexOf(list)  
+   this.NewPro.splice(index,1)
+    this.ProductNumber = this.NewPro.length - this.NewPro.length;
+
+
+      if(list.quantity == 0){
+        return alert("Please add to cart first")
+      }
+      list.quantity--
+      this.Price -= list.price
+
+
+  }
+  countPrice(){
+     this.Price = 0;
+     this.TotalQuantity= 0;
+
+      for(let p of this.productList){
+        this.Price += p.price*p.quantity
+
+
+      }
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,53 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
+
+ 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
+
+
+
+
+
+
